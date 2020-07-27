d6073fc102d1adf0fb91860b060668735b8b7f31
MANDY CHEN (ITA-ISDC-ISD-OOCLL/ZHA) <CHENMA18@OOCL.COM>
2020-07-27 15:45:02 +0800

    [WordFrequencyGame][mandy chen] refactor : rename inputList to wordInfos

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -16,23 +16,23 @@ public class WordFrequencyGame {
         } else {
             try {
                 String[] arr = sentence.split(SPACE_PATTERN);
-                List<WordInfos> inputList = new ArrayList<>();
+                List<WordInfos> wordInfos = new ArrayList<>();
                 for (String s : arr) {
                     WordInfos input = new WordInfos(s, 1);
-                    inputList.add(input);
+                    wordInfos.add(input);
                 }
-                Map<String, List<WordInfos>> map = getListMap(inputList);
+                Map<String, List<WordInfos>> map = getListMap(wordInfos);
                 List<WordInfos> list = new ArrayList<>();
                 for (Map.Entry<String, List<WordInfos>> entry : map.entrySet()) {
                     WordInfos input = new WordInfos(entry.getKey(), entry.getValue().size());
                     list.add(input);
                 }
-                inputList = list;
+                wordInfos = list;
 
-                inputList.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
+                wordInfos.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
 
                 StringJoiner joiner = new StringJoiner(NEW_LINE_DELIMITER);
-                for (WordInfos w : inputList) {
+                for (WordInfos w : wordInfos) {
                     String s = w.getValue() + BLANK_SPACE + w.getWordCount();
                     joiner.add(s);
                 }
