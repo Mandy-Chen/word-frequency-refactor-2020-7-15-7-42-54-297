92db08fbecfaf903bcb53693182c95197ab3ac5a
MANDY CHEN (ITA-ISDC-ISD-OOCLL/ZHA) <CHENMA18@OOCL.COM>
2020-07-27 15:28:33 +0800

    [WordFrequencyGame][mandy chen] refactor :reformat code

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -8,7 +8,7 @@ public class WordFrequencyGame {
     public String getResult(String inputStr) {
 
 
-        if (inputStr.split("\\s+").length==1) {
+        if (inputStr.split("\\s+").length == 1) {
             return inputStr + " 1";
         } else {
 
@@ -22,7 +22,7 @@ public class WordFrequencyGame {
                     inputList.add(input);
                 }
 
-                Map<String, List<Input>> map =getListMap(inputList);
+                Map<String, List<Input>> map = getListMap(inputList);
 
                 List<Input> list = new ArrayList<>();
                 for (Map.Entry<String, List<Input>> entry : map.entrySet()) {
@@ -35,7 +35,7 @@ public class WordFrequencyGame {
 
                 StringJoiner joiner = new StringJoiner("\n");
                 for (Input w : inputList) {
-                    String s = w.getValue() + " " +w.getWordCount();
+                    String s = w.getValue() + " " + w.getWordCount();
                     joiner.add(s);
                 }
                 return joiner.toString();
@@ -47,13 +47,12 @@ public class WordFrequencyGame {
 
     private Map<String, List<Input>> getListMap(List<Input> inputList) {
         Map<String, List<Input>> map = new HashMap<>();
-        for (Input input : inputList){
+        for (Input input : inputList) {
             if (!map.containsKey(input.getValue())) {
                 ArrayList arr = new ArrayList<>();
                 arr.add(input);
                 map.put(input.getValue(), arr);
-            }
-            else {
+            } else {
                 map.get(input.getValue()).add(input);
             }
         }
